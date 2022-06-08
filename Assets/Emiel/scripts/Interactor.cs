05e6d2e818d206478aa24e1f394157d5b0dde184
EmielHo <102023372+EmielHo@users.noreply.github.com>
2022-06-08 11:11:22 +0200

    Generator half done

--- Assets/Emiel/scripts/Interactor.cs
+++ Assets/Emiel/scripts/Interactor.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class Interactor : MonoBehaviour
 {
+    public LayerMask interactableLayermask = 6;
+    public Interactable interactable;
+    public Image interactImage;
+    public Sprite defaultIcon;
+    public Sprite defaultInteractIcon;
+
+    // UnityEvent onInteract;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,38 @@ public class Interactor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit hit;
+
+        if(Physics.Raycast(Camera.main.transform.position,Camera.main.transform.forward, out hit, 2, interactableLayermask))
+        {
+            if (hit.collider.GetComponent<Interactable>() != false)
+            {
+                // onInteract = hit.collider.GetComponent<Interactable>().onInteract;
+                if(interactable == null || interactable.ID != hit.collider.GetComponent<Interactable>().ID) 
+                {
+                    interactable = hit.collider.GetComponent<Interactable>();
+                    // interactImage.sprite = interactable.interactIcon;
+                }
+                if(interactable.interactIcon != null)
+                {
+                    interactImage.sprite = interactable.interactIcon;
+                }
+                else 
+                {
+                    interactImage.sprite = defaultInteractIcon;
+                }
+                if(Input.GetKeyDown(KeyCode.E))
+                {
+                    interactable.onInteract.Invoke();
+                }
+            }
+        }
+        else
+        {
+            if(interactImage.sprite != defaultIcon)
+            {
+                interactImage.sprite = defaultIcon;
+            }
+        }
     }
 }
--- Assets/Emiel/scripts/PlayerInventory.cs
+++ Assets/Emiel/scripts/PlayerInventory.cs
@@ -9,10 +9,24 @@ public class PlayerInventory : MonoBehaviour
    public int NumberOfEels { get; private set; }
 
    public UnityEvent<PlayerInventory> OnEelCollected;
+   
+  
+
 
    public void EelCollected()
    {
        NumberOfEels++;
        OnEelCollected.Invoke(this);
    }
+
+   public void EelDesposited()
+   {
+       if (NumberOfEels > 0) 
+       {
+       NumberOfEels--;
+       OnEelCollected.Invoke(this);
+       }
+   }
+
+
 }
